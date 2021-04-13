706d2c5dc6f216ec51fcb4d4f5d40e7be7d5f343
Diogo0602x <diogoteixeira0602@gmail.com>
2021-04-12 22:58:55 -0300

    new SimpleSlide portfolio

--- web/js/sccript.js
+++ web/js/sccript.js
@@ -1,5 +1,10 @@
-var footer = document.querySelector(".footer_redes");
+new SimpleSlide({ 
+  slide: "quote", // nome do atributo data-slide="principal"
+  time: 5000 // tempo de transição dos slides
+});
 
-footer.innerHTML = "<p>teste</p>"
-
-console.log(footer);
+new SimpleSlide({ 
+  slide: "portfolio",
+  time: 5000,
+  nav: true
+});
