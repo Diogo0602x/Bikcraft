b9a84857adbcb515b53335bf09d25f37ab4d7de2
Diogo0602x <diogoteixeira0602@gmail.com>
2021-04-12 23:06:38 -0300

    if window no script.js

--- web/js/sccript.js
+++ web/js/sccript.js
@@ -1,10 +1,14 @@
-new SimpleSlide({ 
-  slide: "quote", // nome do atributo data-slide="principal"
-  time: 5000 // tempo de transição dos slides
-});
+if(window.SimpleSlide) {
 
-new SimpleSlide({ 
-  slide: "portfolio",
-  time: 5000,
-  nav: true
-});
+  new SimpleSlide({ 
+    slide: "quote", // nome do atributo data-slide="principal"
+    time: 5000 // tempo de transição dos slides
+  });
+
+  new SimpleSlide({ 
+    slide: "portfolio",
+    time: 5000,
+    nav: true
+  });
+
+}
