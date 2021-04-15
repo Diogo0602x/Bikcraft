f595b7f6c988f35de10ec294fa96dc0ae5e26cf0
Diogo0602x <diogoteixeira0602@gmail.com>
2021-04-14 21:48:01 -0300

    javascript feito no index.html

--- web/js/sccript.js
+++ web/js/sccript.js
@@ -1,18 +1,18 @@
-if(window.SimpleSlide) {
+if (window.SimpleSlide) {
 
-  new SimpleSlide({ 
-    slide: "quote", // nome do atributo data-slide="principal"
-    time: 5000 // tempo de transição dos slides
-  });
+	new SimpleSlide({
+		slide: "quote", // nome do atributo data-slide="principal"
+		time: 5000 // tempo de transição dos slides
+	});
 
-  new SimpleSlide({ 
-    slide: "portfolio",
-    time: 5000,
-    nav: true
-  });
-  
+	new SimpleSlide({
+		slide: "portfolio",
+		time: 5000,
+		nav: true
+	});
+
 }
 
-if(window.SimpleAnime) {
-  new SimpleAnime();
+if (window.SimpleAnime) {
+	new SimpleAnime();
 }
